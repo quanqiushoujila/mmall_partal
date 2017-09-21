e08d25f68222e3f434ba2796e46f78a47cf7712a
k <k@k.com>
2017-09-21 14:46:24 +0800

    通用模块

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,10 +2,12 @@
 * @Author: kai
 * @Date:   2017-09-20 09:40:51
 * @Last Modified by:   kai
-* @Last Modified time: 2017-09-21 13:04:21
+* @Last Modified time: 2017-09-21 13:27:51
 */
 'use strict'
 
 require('normalize.css');
 require('./layout.scss');
 require('node_modules/font-awesome/css/font-awesome.min.css');
+
+require('./nav-simple/nav-simple.js');
---
+++ src/page/common/nav-simple/nav-simple.js
@@ -0,0 +1,7 @@
+/*
+* @Author: kai
+* @Date:   2017-09-21 13:25:14
+* @Last Modified by:   kai
+* @Last Modified time: 2017-09-21 13:27:29
+*/
+require('./nav-simple.scss');
