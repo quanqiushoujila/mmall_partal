2e4081b5cebc867c98487cb6db84521c5a43623c
k <k@k.com>
2017-10-23 16:57:37 +0800

    登录页面输出

---
+++ src/page/user-login/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: kai
+* @Date:   2017-10-23 16:27:31
+* @Last Modified by:   kai
+* @Last Modified time: 2017-10-23 16:29:37
+*/
+'use strict';
+
+require('./index.scss');
+require('page/common/nav-simple/index.js');
+var _mm = require('util/mm.js');
