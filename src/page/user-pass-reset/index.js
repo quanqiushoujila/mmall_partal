7f8cff4f41ea729dd35dae56051304fa8608f464
k <k@k.com>
2017-10-24 16:33:21 +0800

    忘记密码

---
+++ src/page/user-pass-reset/index.js
@@ -0,0 +1,121 @@
+/*
+* @Author: kai
+* @Date:   2017-10-24 14:17:25
+* @Last Modified by:   kai
+* @Last Modified time: 2017-10-24 15:44:01
+*/
+'use strict';
+
+require('./index.scss');
+require('page/user-login/index.scss');
+require('page/common/nav-simple/index.js');
+var _mm = require('util/mm.js');
+var _user = require('service/user-service.js');
+var _notice = require('js/notice.js');
+
+var page = {
+  data: {
+    username: '',
+    answer: '',
+    question: '',
+    token: ''
+  },
+
+  init: function () {
+    this.onLoad();
+    this.bindEvent();
+  },
+
+  // 事件绑定
+  bindEvent: function () {
+    var _this = this;
+
+    // 第一步 验证用户名是否存在
+    $('#submit-username').click(function () {
+      var username = $.trim($('#username').val());
+
+      if (username) {
+        _user.getQuestion(username, function (res) {
+          _this.data.username = username;
+          _this.data.question = res.data;
+          $('#question').text(res.data)
+          _this.loadStepQuestion();
+        }, function (err) {
+          _notice.formError.show(err);
+        })
+      } else {
+        _notice.formError.show('用户名不能为空');
+      }
+    });
+
+    // 第二步 验证问题回答的是否正确
+    $('#submit-question').click(function () {
+      var answer = $('#answer').val();
+      if (answer) {
+        _user.checkAnswer({
+          answer: answer,
+          username: _this.data.username,
+          question: _this.data.question
+        }, function (res) {
+          _this.data.answer = answer;
+          _this.data.token = res.data
+          _this.loadStepPassword();
+        }, function (err) {
+          _notice.formError.show(err);
+        });
+      } else {
+        _notice.formError.show('答案不能为空');
+      }
+    });
+
+    // 第三步 新密码提交
+    $('#submit-password').click(function () {
+      var password = $.trim($("#password").val());
+
+      if (password) {
+        _user.checkAnswer({
+          forgetToken: _this.data.token,
+          username: _this.data.username,
+          passwordNew: password
+        }, function (res) {
+          window.location.href = './index.html';
+        }, function (err) {
+          _notice.formError.show(err);
+        });
+      } else {
+        _notice.formError.show('密码不能为空');
+      }
+    });
+  },
+
+  onLoad: function () {
+    this.loadStepUsername();
+  },
+
+  // 第一步 输入用户名
+  loadStepUsername: function () {
+    $('.step-username').show();
+  },
+
+  // 第二步 输入回答问题
+  loadStepQuestion: function () {
+    // 清除错误提示
+    _notice.formError.hide();
+
+    $('.step-username').hide().
+      siblings('.step-question').show().
+      find('#question').text(this.data.question);
+  },
+
+  // 第三部 输入新密码
+  loadStepPassword: function () {
+    // 清除错误提示
+    _notice.formError.hide();
+
+    $('.step-question').hide().sibling('.step-passsword').show();
+  }
+}
+
+$(function () {
+  page.init();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: kai
 * @Date:   2017-08-09 15:43:54
 * @Last Modified by:   kai
-* @Last Modified time: 2017-10-24 09:15:20
+* @Last Modified time: 2017-10-24 14:17:51
 */
 // var path                = require('path')
 var webpack             = require('webpack');
@@ -44,7 +44,7 @@ var config = {
         // 'cart'              : ['./src/page/cart/index.js'],
         'user-login'        : ['./src/page/user-login/index.js'],
         'user-register'     : ['./src/page/user-register/index.js'],
-        // 'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
+        'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
         // 'user-center'       : ['./src/page/user-center/index.js'],
         // 'user-center-update': ['./src/page/user-center-update/index.js'],
         // 'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
@@ -124,7 +124,7 @@ var config = {
         // new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
-        // new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
         // new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
         // new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         // new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
